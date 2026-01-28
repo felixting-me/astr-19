81a71b332079ad32940a33e7952c9b06315aa0c1
felixting-me <fkting@ucsc.edu>
2026-01-28 13:49:44 -0800

    Adding Code Journal 1 Prompt 1

---
+++ Session-1-Prompt.py
@@ -0,0 +1,8 @@
+def main():
+	print("Felix Ting")
+	print("He/Him")
+	print("Movie: La Haine, this is an old french movie that captures the racial/ploitical injustice that someone lving in the 'banlieu' can experience.")
+	print("Food: sushi, it's just really good and never bad.")
+
+if __name__ == "__main__":
+	main()
